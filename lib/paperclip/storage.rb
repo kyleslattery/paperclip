e061203a65d0cbab2121cbdd92487fec7b235737
Kyle Slattery <kyle@kyleslattery.com>
2009-02-11 02:34:18 -0500

    Initial work at adding SFTP as storage option

--- lib/paperclip/storage.rb
+++ lib/paperclip/storage.rb
@@ -71,6 +71,68 @@ module Paperclip
         @queued_for_delete = []
       end
     end
+    
+    module Sftp
+      def self.extended(base)
+        require 'net/ssh'
+        require 'net/sftp'
+        
+        base.instance_eval do
+          # TODO: Get this from options
+          @host = 'static.kyleslattery.com'
+          @user = 'kyleslat'
+        end
+      end
+      
+      def ssh
+        @ssh_connection ||= Net::SSH.start(@host, @user)
+      end
+      
+      def exists?(style = default_style)
+        ssh.exec!("ls #{path(style)} 2>/dev/null") ? true : false
+      end
+      
+      def to_file(style=default_style)
+        @queued_for_write[style] || (ssh.sftp.file.open(path(style), 'rb') if exists?(style))
+      end
+      alias_method :to_io, :to_file
+      
+      def flush_writes #:nodoc:
+        logger.info("[paperclip] Writing files for #{name}")
+        @queued_for_write.each do |style, file|
+          file.close
+          ssh.exec! "mkdir -p #{File.dirname(path(style))}"
+          logger.info("[paperclip] -> #{path(style)}")
+          puts 'uploading'
+          ssh.sftp.upload!(file.path, path(style))
+          puts 'setting stat'
+          ssh.sftp.setstat!(path(style), :permissions => 0644)
+        end
+        @queued_for_write = {}
+      end
+
+      def flush_deletes #:nodoc:
+        logger.info("[paperclip] Deleting files for #{name}")
+        @queued_for_delete.each do |path|
+          begin
+            logger.info("[paperclip] -> #{path}")
+            ssh.sftp.remove(path)
+            FileUtils.rm(path) if File.exist?(path)
+          rescue Net::SFTP::StatusException
+            # ignore file-not-found, let everything else pass
+          end
+          begin
+            while(true)
+              path = File.dirname(path)
+              sftp.rmdir(path)
+            end
+          rescue Net::SFTP::StatusException
+            # Stop trying to remove parent directories
+          end
+        end
+        @queued_for_delete = []
+      end
+    end
 
     # Amazon's S3 file hosting service is a scalable, easy place to store files for
     # distribution. You can find out more about it at http://aws.amazon.com/s3
